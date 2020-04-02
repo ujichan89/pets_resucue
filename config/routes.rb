8d74d73ea1f2479a2a756f29841e64ad37d9232a
ujichan89 <bbl.029.uji@gmail.com>
2020-04-02 20:35:40 +0900

    tweet(new/create)機能だけ

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root "window#index"
-  resources :tweets, only:[:index]
+  resources :tweets, only:[:index, :new, :create]
   resources :blog, only:[:index, :new, :create, :show, :edit, :update, :destroy] 
 end
